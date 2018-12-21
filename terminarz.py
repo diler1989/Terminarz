bbb886a0783fcf4efd7ca672697cbfbbc22ab550
diler1989 <45041861+diler1989@users.noreply.github.com>
2018-12-21 16:27:33 +0100

    Add files via upload

--- terminarz.py
+++ terminarz.py
@@ -1,78 +1,74 @@
-import shelve, sys, calendar
+import shelve
+import datetime
 
-def body():
-    print('*'*90)
-
-def cal():
-    body()
-    print(calendar.month(2018, 12))
-
+mydate = datetime.datetime.now()
+now = mydate.strftime("%B")
+days_in_month_dict = {"January": 31, "February": 28,
+                      "March": 31, "April": 30,
+                      "May": 31, "June": 30,
+                      "July": 31, "August": 31,
+                      "September": 30, "October": 31,
+                      "November": 30, "December": 31}
 def init():
-
-    body()
 
     global baza
 
     try:
-        baza = shelve.open('baza_zadan')
+        baza = shelve.open('_zadan')
     except:
-        print ('Błąd krytyczny! Baza danych nie została otwarta!\n')
-        sys.exit(0)
-    print ('Inicjalizacja udana.\nBaza danych została otwarta!')
+        print('Błąd krytyczny! Baza danych nie została otwarta!\n')
+        exit(0)
+    print('Inicjalizacja udana.\nBaza danych została otwarta!\n')
+    tab = input('Utworzyc kalendarz? [T][N]: ')
+    if tab == 'T':
+        crate_data()
+    elif tab == 'N':
+        menu()
+def crate_data():
+    for day in range(1, 32):
+        data = str(day)
+        task = ' '
+        if data not in baza.keys():
+            baza[data] = task
+    print ("Wprowadzono.\n")
 
 def add_task():
 
-    body()
+    global baza, data, task
 
-    global baza
+    data = input('Podaj date: ')
 
-    data = input('Podaj date (dd-mm-rrrr): ')[:10]
-
-    if len(data) < 9:
-        return
-        print('nieprawidlowa data!')
-
-    if data in baza.keys() or data == '' in baza.keys():
-        print('Podany termin juz zostal wprowadzony!\n')
-        task=input('Dodaj kolejne zadanie do dnia: ')
+    if data in baza.keys():
+        task = input('Jakie kolejne zadanie dodac do dnia: ')
         mem = baza[data]
         baza[data] = mem+', '+task
-    else:
-        data
-        task =input('Jakie zadania zapamietac: ')
+        print('Poprawnie dodano zadanie do dnia: ', data, now)
 
-    if data not in baza.keys():
-        baza[data]= task
-        body()
-        print ("Wprowadzono.\n")
+def delete():
+    global baza
+    data = input('Zadanie z jakiego dnia usunac?: ')
+    if data in baza.keys():
+        baza[data] = ' '
 
 def wiev():
 
-    global baza
-
-    body()
+    global baza, data, task
+    print("*" * 90)
     print ('Zadania w terminarzu'.center(90))
     print ('-'*90)
     print ('|'+'DATA'.center(40)+'|'+'ZADANIE'.center(47)+'|')
     print ('-'*90)
 
     for data,task in baza.items():
-        print ('|'+data.center(40)+'|'+task.center(47)+'|')
+        print ('| '+data.center(10),now.center(28)+'|'+task.center(47)+'|')
     print ('-'*90)
-    body()
-
-def delete():
-    global baza
-    data = input('Zadanie z jakiego dnia usunac?: ')
-    if data in baza.keys():
-        del baza[data]
 
 def menu():
     while True:
+        print('*' * 90)
+        print('|'+'Terminarz'.center(90))
         print("*" * 90)
-        print("Terminarz".center(90))
-        print("*" * 90)
-        print("[D]Dodaj zadanie [W]Wyswietl zadania [U]Usun zadanie [C]Kalendarz [K]Koniec".center(90))
+        print('|'+"[D]Dodaj zadanie   [W]Wyswietl zadania   [U]Usun zadanie   [K]Koniec".center(90))
         print("*" *90)
         db = input(">:").upper()
         if db == 'D':
@@ -81,16 +77,10 @@ def menu():
             wiev()
         elif db == "U":
             delete()
-        elif db == "C":
-            cal()
         elif db == "K":
             baza.close()
             print('Wylogowano z bazy.')
             exit(0)
-
 init()
 menu()
-
-
-
-
+baza.close()
